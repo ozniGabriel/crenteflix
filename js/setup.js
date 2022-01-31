0b9ea7f2a24884cf594241afbe4d929566eea28c
ozni <89555336+ozniGabriel@users.noreply.github.com>
2022-01-31 14:10:02 -0300

    first

---
+++ js/setup.js
@@ -0,0 +1,30 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:6,
+  autoplay: true,
+  autoplayTimeout: 2000,
+  autoplayHoverPause: true,
+  smartSpeed: 3000,
+  slideTransition: 'ease-out',
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
+function ativarMenuHamburguer(){
+    let a = document.querySelector('.leftElementsMenu').style["display"]
+    if(a !== "flex"){
+        document.querySelector('.leftElementsMenu').style = "display: flex;"
+    } else {
+        document.querySelector('.leftElementsMenu').style = "display: none"
+    }
+
+}
